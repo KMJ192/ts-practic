a1da704207fc54df8ed41d3e0fec10a5a036eb59
MyeongJunKim <myeongjun222@naver.com>
2022-06-01 23:10:09 +0900

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,2 +1,9 @@
 // import AutoCompletion from '@src/Algorithm/AutoCompletion';
 // AutoCompletion();
+import lruCache from '@src/Algorithm/LRUCache/LRUCache';
+
+function App() {
+  lruCache();
+}
+
+App();

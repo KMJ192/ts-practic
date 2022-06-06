ebe047454e1a566e09765d82a2d6c4c53042ab32
MyeongJunKim <myeongjun222@naver.com>
2022-06-06 23:46:14 +0900

    Update LFUCache.ts

--- src/Algorithm/LFUCache/LFUCache.ts
+++ src/Algorithm/LFUCache/LFUCache.ts
@@ -1,9 +1,3 @@
-type Tuple<T, N> = Array<T | N>;
-
-const MakePair = <T, N>(a: T, b: N): Tuple<T, N> => {
-  return [a, b];
-};
-
 class DLLNode<T> {
   public prev: DLLNode<T> | null;
   public next: DLLNode<T> | null;
@@ -29,13 +23,13 @@ class DLLNode<T> {
 class Frequncy {
   public val: number;
 
-  private refCount: number;
+  public refCount: number;
 
   private freqMap: Map<number, DLLNode<number>>;
 
-  public head: DLLNode<number> | null;
+  public front: DLLNode<number> | null;
 
-  public tail: DLLNode<number> | null;
+  public rear: DLLNode<number> | null;
 
   constructor(value: number) {
     this.val = value;
@@ -44,14 +38,22 @@ class Frequncy {
 
     this.freqMap = new Map();
 
-    this.head = null;
+    this.front = null;
 
-    this.tail = null;
+    this.rear = null;
   }
 
-  public removeKey(key: number) {
+  public remove(key: number) {
     if (this.freqMap.has(key)) {
       const oldNode = this.freqMap.get(key) as DLLNode<number>;
+      const prev = oldNode.prev;
+      const next = oldNode.next;
+      if (this.rear === oldNode) {
+        this.rear = prev;
+      }
+      if (this.front === oldNode) {
+        this.front = next;
+      }
 
       oldNode.disconnect();
 
@@ -62,32 +64,22 @@ class Frequncy {
 
   public push_back(value: number) {
     const newNode = new DLLNode(value);
-    if (this.tail === null) {
-      this.head = newNode;
+    if (this.rear === null) {
+      this.front = newNode;
     } else {
-      this.tail.next = newNode;
-      newNode.prev = this.tail;
+      this.rear.next = newNode;
+      newNode.prev = this.rear;
     }
-    this.tail = newNode;
+    this.rear = newNode;
+    this.refCount += 1;
 
-    return this.tail;
+    return this.rear;
   }
 
   public pop_front() {
-    if (this.head === null) throw Error('Access empty memory');
-
-    const curNode = this.head;
-    if (this.head.next === null) {
-      this.head = null;
-      this.tail = null;
-    } else {
-      this.head.next.prev = null;
-      this.head = this.head.next;
-    }
-
-    curNode.next = null;
-    curNode.prev = null;
-    return curNode;
+    if (this.front === null) throw Error('Access empty memory');
+    const key = this.front.value;
+    this.remove(key);
   }
 }
 
@@ -115,19 +107,45 @@ class LFUCache {
     this.freq = new Map();
   }
 
-  get(key: number): number {
-    return 0;
-  }
-
-  put(key: number, value: number): void {
+  public get(key: number): number {
     if (this.dataInfo.has(key)) {
       const item = this.dataInfo.get(key) as CacheTable;
-      const oldFreq = this.freq.get(key) as Frequncy;
-      oldFreq.removeKey(key);
+      const oldFreq = this.freq.get(item.freq) as Frequncy;
+      oldFreq.remove(key);
+
+      if (this.lowestFreq === oldFreq.val && oldFreq.refCount === 0) {
+        this.lowestFreq += 1;
+      }
+      item.freq += 1;
+
+      const curFreq = new Frequncy(item.freq);
+      curFreq.push_back(key);
+      this.freq.set(item.freq, curFreq);
+
+      return item.value;
+    }
+
+    return -1;
+  }
+
+  public put(key: number, value: number): void {
+    if (this.dataInfo.has(key)) {
+      // cache에 데이터가 있음
+      const item = this.dataInfo.get(key) as CacheTable;
+      const oldFreq = this.freq.get(item.freq) as Frequncy;
+      oldFreq.remove(key);
+
+      item.freq += 1;
+      item.value = value;
+
+      const f = new Frequncy(item.freq);
+      f.push_back(key);
+      this.freq.set(key, f);
     } else if (this.capacity > 0) {
+      // cache 메모리에 데이터가 없음
       if (this.length === this.capacity) {
-        const lowestFreq = this.freq.get(key) as Frequncy;
-        this.dataInfo.delete((lowestFreq.head as DLLNode<number>).value);
+        const lowestFreq = this.freq.get(this.lowestFreq) as Frequncy;
+        this.dataInfo.delete((lowestFreq.front as DLLNode<number>).value);
         lowestFreq.pop_front();
         this.length -= 1;
       }
@@ -140,6 +158,7 @@ class LFUCache {
       const curFreq = new Frequncy(1);
       curFreq.push_back(key);
       this.freq.set(1, curFreq);
+
       this.length += 1;
       this.lowestFreq = 1;
     }
@@ -147,8 +166,17 @@ class LFUCache {
 }
 
 function lfuCache() {
-  const obj = new LFUCache(4);
-  const data = [0, 1, 1, 2, 0, 9, 8, 2, 1];
+  const cache = new LFUCache(2);
+  console.log(cache.put(1, 1)); // 1: 1
+  console.log(cache.put(2, 2)); // 1: 1, 2: 1
+  console.log(cache.get(1)); // 1, 1: 2, 2: 1
+  console.log(cache.put(3, 3)); // 1: 2, 3: 1
+  console.log(cache.get(2)); // -1, 1: 2, 3: 1
+  console.log(cache.get(3)); // 3, 1: 2, 3: 2
+  console.log(cache.put(4, 4)); // 3: 2, 4: 1
+  console.log(cache.get(1)); // -1
+  console.log(cache.get(3)); // 3
+  console.log(cache.get(4)); // 4
 }
 
 export default lfuCache;
